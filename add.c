993c2e95dd8966d884efd73497d4273cdaf6925f
Notyasser <showhad55@gmail.com>
2023-11-14 13:34:40 -0800

    fixed add.c

--- add.c
+++ add.c
@@ -1,5 +1,18 @@
 #include "monty.h"
 
+/**
+ * add - Adds the top two elements of the stack.
+ * @stack: A pointer to the top of the stack.
+ * @line_number: The current line number in the Monty file.
+ *
+ * Description:
+ * Checks if the stack has at least two elements.
+ * If not, prints an error message and exits the program.
+ * Adds the values of the top two elements and updates the result.
+ * Removes the top element from the stack.
+ *
+ * Return: None.
+ */
 void add(stack_t **stack, unsigned int line_number)
 {
     if (*stack == NULL || (*stack)->next == NULL)
@@ -14,4 +27,3 @@ void add(stack_t **stack, unsigned int line_number)
     (*stack)->next->n += (*stack)->n;
     pop(stack, line_number);
 }
-
