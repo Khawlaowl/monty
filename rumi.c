6a06e21d2679ae5da6164b7f5fd31d7ae37130cc
Khawlaowl <138932395+Khawlaowl@users.noreply.github.com>
2023-11-15 20:21:14 +0100

    Update and rename pchar

--- rumi.c
+++ rumi.c
@@ -11,13 +11,15 @@
  * Checks if the value at the top of the stack is within the valid ASCII range.
  * If not, prints an error message and exits the program.
  * Prints the character corresponding to the value at the top of the stack.
- *
  * Return: None.
  */
 
 void pchar(stack_t **stack, unsigned int line_number)
 {
-	if (*stack == NULL)
+	stack_t *current = *stack;
+	
+	
+	if (!current)
 	{
 		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
 		fclose(bus.file);
@@ -26,7 +28,7 @@ void pchar(stack_t **stack, unsigned int line_number)
 		exit(EXIT_FAILURE);
 	}
 
-	if ((*stack)->n < 0 || (*stack)->n > 127)
+if (current->n > 127 || current->n < 0)
 	{
 		fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
 		fclose(bus.file);
