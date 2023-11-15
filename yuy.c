d485b7f20b260eb21c2b3a2f3a87418b6f31a403
Khawlaowl <138932395+Khawlaowl@users.noreply.github.com>
2023-11-15 20:02:56 +0100

    rotr

--- yuy.c
+++ yuy.c
@@ -15,23 +15,5 @@
 
 void rotr(stack_t **stack, unsigned int line_number)
 {
-	stack_t *temp, *last;
 
-	(void)line_number;
-
-	if (*stack != NULL && (*stack)->next != NULL)
-	{
-		last = *stack;
-
-		while (last->next != NULL)
-			last = last->next;
-
-		temp = last->prev;
-		temp->next = NULL;
-		last->prev = NULL;
-		last->next = *stack;
-		(*stack)->prev = last;
-		*stack = last;
-	}
-}
 
