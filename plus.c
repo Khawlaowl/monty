2ae481f5021a7539fa9f4add1b54b22433cc1c93
Khawlaowl <138932395+Khawlaowl@users.noreply.github.com>
2023-11-15 20:09:04 +0100

    add

---
+++ plus.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+ * add - Adds the top two elements of the stack.
+ * @stack: A pointer to the top of the stack.
+ * @counter: The current line number in the Monty file.
+ *
+ * Description:
+ * Checks if the stack has at least two elements.
+ * If not, prints an error message and exits the program.
+ * Adds the values of the top two elements and updates the result.
+ * Removes the top element from the stack.
+ *
+ * Return: None.
+ */
