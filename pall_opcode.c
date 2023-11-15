8ebdeb85f08b145ea34bdea9942b40d62a37ddad
Notyasser <showhad55@gmail.com>
2023-11-16 00:22:35 +0100

    implement push & pall

--- pall_opcode.c
+++ pall_opcode.c
@@ -3,7 +3,7 @@
 /**
  * pall - Prints all the elements of the stack.
  *
- * This function prints the integer data of all the nodes in the stack, starting
+ * This function prints the integer data of all nodes in the stack, starting
  * from the top. The line_number parameter is not used in this function.
  *
  * @stack: A pointer to the pointer to the stack's top node.
