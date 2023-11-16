25d90e22191cd584ece971dc10439346f2ac9004
Notyasser <showhad55@gmail.com>
2023-11-17 00:15:28 +0100

    fixing

--- errors.c
+++ errors.c
@@ -1,58 +1,58 @@
 #include "monty.h"
 
 /**
- * pop_coderr - Handles an error when attempting to pop from an empty stack.
+ * pop_error - Handles an error when attempting to pop from an empty stack.
  *
  * This function is called when an attempt is made to pop an element from an
  * empty stack. It prints an error message indicating the line number and
  * exits the program with a failure status.
  *
- * @ln_number: The line number from the Monty file where the error occurred.
+ * @line_number: The line number from the Monty file where the error occurred.
  */
-void pop_coderr(unsigned int ln_number)
+void pop_error(unsigned int line_number)
 {
-	fprintf(stderr, "L%d: can't pop an empty stack\n", ln_number);
+	fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 	exit(EXIT_FAILURE);
 }
 
 /**
- * swap_coderr - Handle an error when attempt to swap element in a short stack
+ * swap_error - Handle an error when attempt to swap element in a short stack
  *
  * This function is called when an attempt is made to swap elements in a stack
  * that contains fewer than two elements. It prints an error message indicating
  * the line number and exits the program with a failure status.
  *
- * @ln_number: The line number from the Monty file where the error occurred.
+ * @line_number: The line number from the Monty file where the error occurred.
  */
-void swap_coderr(unsigned int ln_number)
+void swap_error(unsigned int line_number)
 {
-	fprintf(stderr, "L%d: can't swap, stack too short\n", ln_number);
+	fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
 	exit(EXIT_FAILURE);
 }
 
 /**
- * add_coderr - Handle error when attempt to add elements in short stack
+ * add_error - Handle error when attempt to add elements in short stack
  *
  * This function is called when an attempt is made to add elements in a stack
  * that contains fewer than two elements. It prints an error message indicating
  * the line number and exits the program with a failure status.
  *
- * @ln_number: The line number from the Monty file where the error occurred.
+ * @line_number: The line number from the Monty file where the error occurred.
  */
-void add_coderr(unsigned int ln_number)
+void add_error(unsigned int line_number)
 {
-	fprintf(stderr, "L%d: can't add, stack too short\n", ln_number);
+	fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
 	exit(EXIT_FAILURE);
 }
 
 /**
- * malloc_coderr - Handles an error when memory allocation (malloc) fails.
+ * malloc_error - Handles an error when memory allocation (malloc) fails.
  *
  * This function is called when an attempt to allocate memory using malloc
  * fails. It prints an error message indicating the failure and exits the
  * program with a failure status.
  */
-void malloc_coderr(void)
+void malloc_error(void)
 {
 	fprintf(stderr, "Error: malloc failed\n");
 	exit(EXIT_FAILURE);
--- errors1.c
+++ errors1.c
@@ -1,48 +1,48 @@
 #include "monty.h"
 
 /**
- * push_coderr - Handle  error when the push command has an invalid argument.
+ * push_error - Handle  error when the push command has an invalid argument.
  *
  * This function is called when an attempt to execute the 'push' command with
  * an invalid argument (non-integer) is encountered in the Monty file. It
  * prints an error message indicating the line number, closes the file, and
  * exits the program with a failure status.
  *
- * @ln_number: The line number from the Monty file where the error occurred.
- * @file_name: A pointer to the Monty file being processed.
+ * @line_number: The line number from the Monty file where the error occurred.
+ * @file: A pointer to the Monty file being processed.
  */
-void push_coderr(unsigned int ln_number, FILE *file_name)
+void push_error(unsigned int line_number, FILE *file)
 {
-	fprintf(stderr, "L%d: usage: push integer\n", ln_number);
-	fclose(file_name);
+	fprintf(stderr, "L%d: usage: push integer\n", line_number);
+	fclose(file);
 	exit(EXIT_FAILURE);
 }
 
 /**
- * pall_coderr - Placeholder function for handling errors during 'pall'.
+ * pall_error - Placeholder function for handling errors during 'pall'.
  *
  * This function is intentionally designed to do nothing and serves as a
  * placeholder for handling errors during the 'pall' command. It is called
  * when an error condition related to 'pall' is detected.
  */
-void pall_coderr(void)
+void pall_error(void)
 {
 	return; /* Do nothing */
 }
 
 /**
- * pint_coderr - Handle "pint" command when the stack is empty.
+ * pint_error - Handle "pint" command when the stack is empty.
  *
  * This function is called when an attempt is made to execute the 'pint'
  * command on an empty stack. It prints an error message indicating the
  * line number, and exits the program with a failure status.
  *
- * @ln_number: The line number where the error occurred.
+ * @line_number: The line number where the error occurred.
  * Return: No return value; prints an error message and exits with failure
  * status.
  */
-void pint_coderr(unsigned int ln_number)
+void pint_error(unsigned int line_number)
 {
-	fprintf(stderr, "L%d: can't pint, stack empty\n", ln_number);
+	fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
 	exit(EXIT_FAILURE);
 }
