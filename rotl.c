47a0ec24ceca51997d95c98e33aa7b7cbd662a76
Notyasser <showhad55@gmail.com>
2023-11-16 15:59:16 -0800

    fixing

--- rotl.c
+++ rotl.c
@@ -7,19 +7,19 @@
  */
 void f_rotl(stack_t **head, __attribute__((unused)) unsigned int counter)
 {
-if (*head == NULL || (*head)->next == NULL)
-{
-return;
-}
-stack_t *last;
-last= *head;
-while (last->next != NULL)
-{
-last = last->next;
-}
-last->next = *head;
-(*head)->prev = last;
-*head = (*head)->next;
-(*head)->prev = NULL;
-last->next->next = NULL;
+	if (*head == NULL || (*head)->next == NULL)
+	{
+		return;
+	}
+	stack_t *last;
+	last= *head;
+	while (last->next != NULL)
+	{
+		last = last->next;
+	}
+	last->next = *head;
+	(*head)->prev = last;
+	*head = (*head)->next;
+	(*head)->prev = NULL;
+	last->next->next = NULL;
 }
